d5370a4c93681ed24753c855918131e2ea9f67cb
Stathis Vlachos <stathvlach@posteo.com>
2026-06-01 09:51:35 +0300

    Fix typo

--- sql/GOLD_CREATE_TABLES.sql
+++ sql/GOLD_CREATE_TABLES.sql
@@ -1,9 +1,9 @@
 CREATE TABLE total_hours_per_project (
     project_id TEXT PRIMARY KEY,
-    total_hours REAL NOT NULL CHECK (hours >= 0)
+    total_hours REAL NOT NULL CHECK (total_hours >= 0)
 );
 
 CREATE TABLE total_hours_per_employee (
     employee_id TEXT PRIMARY KEY,
-    total_hours REAL NOT NULL CHECK (hours >= 0)
+    total_hours REAL NOT NULL CHECK (total_hours >= 0)
 );
